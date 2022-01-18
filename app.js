383c9887f388d3e5a40e91238fd286d2dc3675d1
Kimyeonho <kyh0703@nate.com>
2022-01-18 12:46:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const title1 = document.getElementsByClassName("hello");
 
 // css selector
+// 첫번째꺼만 가져온다
 const title = document.querySelector(".hello h1");
+
+// css selector
+// 첫번째꺼만 가져온다
+const titleAll = document.querySelectorAll(".hello h1");
 
 console.log(title);
