5af78bf5b6b8863c9b100190782124abd7046b67
Kimyeonho <kyh0703@nate.com>
2022-01-19 21:36:11 +0900

    Add Backtick

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(event);
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  // Backtick ` ``
+  // 1. $(변수명)
+  // 2. ` `시작해야됨
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove("hidden");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
