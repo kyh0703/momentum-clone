95890b7eb327e81a9444ba0a0d8cc5f6f7d0e445
Kimyeonho <kyh0703@nate.com>
2022-01-19 12:48:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginBtn = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
-  // validate 1. 방식 JS 보단 Form에서 해결할수있으면 해결
-  // if (username === "") {
-  //   alert("please write your name");
-  // } else if (15 < username.length) {
-  //   alert("Your name is too long.");
-  // }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
 }
 
-loginBtn.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
