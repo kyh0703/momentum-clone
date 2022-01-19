28d2d037c358e46e64540b1505348e24f82ebf5d
Kimyeonho <kyh0703@nate.com>
2022-01-19 12:40:36 +0900

    add submit

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginBtn = document.querySelector("#login-form button");
 
-// == 연산자는 작업자가 원치 않는 강제 형변환을 실행하고 그것이 다른 언어의 ==연산자와는 차이가 있는 동작이므로 ==를 ===로 컴파일한다는 것이다.
-// ClassList는 전체를 다 변경하지 않고 HTML element 의 클래스 리스트를 가져옴
-function handleTitleClick() {
-  // const clickedClass = "clicked";
-  // if (h1.classList.contains(clickedClass)) {
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  console.log(username);
+  // validate 1. 방식 JS 보단 Form에서 해결할수있으면 해결
+  // if (username === "") {
+  //   alert("please write your name");
+  // } else if (15 < username.length) {
+  //   alert("Your name is too long.");
   // }
-  // => toggle
-  h1.classList.toggle("clicked");
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginBtn.addEventListener("click", onLoginBtnClick);
