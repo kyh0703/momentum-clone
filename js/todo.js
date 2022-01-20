6ba7e000464d25de83ba6b417a18aa43f975ce09
Kimyeonho <kyh0703@nate.com>
2022-01-20 23:15:49 +0900

    모멘텀 완료!
    CSS 변경해보기

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+let toDos = [];
+
+const TODOS_KEY = "todos";
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos(toDos);
 }
 
 function paintTodo(newTodo) {
@@ -12,7 +22,8 @@ function paintTodo(newTodo) {
   const span = document.createElement("span");
   const btn = document.createElement("button");
 
-  span.innerText = newTodo;
+  li.id = newTodo.id;
+  span.innerText = newTodo.text;
   btn.innerText = "❌";
   btn.addEventListener("click", deleteTodo);
 
@@ -24,8 +35,23 @@ function paintTodo(newTodo) {
 function handleTodoSubmit(event) {
   event.preventDefault(event);
   const newTodo = todoInput.value;
-  paintTodo(newTodo);
   todoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveToDos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos !== null) {
+  const parsedToDos = JSON.parse(savedTodos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
+
+function sexyFilter() {}
