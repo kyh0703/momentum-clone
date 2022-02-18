6d43b92542ad35fa98fc13a8eeee2ae2f365a35a
Kimyeonho <kyh0703@nate.com>
2022-02-18 22:59:18 +0900

    add body background image

--- js/background.js
+++ js/background.js
@@ -4,6 +4,4 @@
 
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const todayBgImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `img/${todayBgImage}`;
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${todayBgImage})`;
